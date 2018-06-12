d6356b4c7b21d3d7fec2225f0031d79072c215f2
gregory hanffou <ghanffou@gmail.com>
2018-06-12 14:58:27 +0200

    spaces diferences

---
+++ app/controllers/drones_controller.rb
@@ -0,0 +1,49 @@
+class DronesController < ApplicationController
+  before_action :set_drone, only: [ :show, :edit, :update, :destroy]
+
+  def index
+    @drones = Drone.all
+  end
+
+  def show
+  end
+
+  def new
+    @drone = Drone.new
+  end
+
+  def create
+    @drone = Drone.new(drone_params)
+    if @drone.save
+      redirect_to drone_path(@drone)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @drone.update(drone_params)
+    redirect_to drone_path(@drone)
+  end
+
+  def destroy
+    @drone.destroy
+    redirect_to drones_path
+  end
+
+
+  private
+
+  def set_drone
+    @drone = Drone.find(params[:id])
+  end
+
+  def drone_params
+    params.require(:drone).permit(:name, :price, :location, :photo)
+  end
+
+end
+
