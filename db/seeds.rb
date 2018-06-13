230d062a9e7ae6269548726b507ad8a261965f78
Jason Everaert <eventlessdrop8@gmail.com>
2018-06-13 10:58:38 +0200

    made card on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,31 +14,31 @@ User.destroy_all
 puts 'making record'
 
 5.times do
-  #creating the parameters for the user
+  # creating the parameters for the user
   email =  Faker::Internet.email
   password = Faker::HowIMetYourMother.catch_phrase
   name = Faker::Ancient.god
   photo = Faker::GameOfThrones.character
   description = Faker::GameOfThrones.quote
 
-  #creating an instance of the user
-  u = User.create({email:email, password:password, name:name, photo:photo, description:description})
-  #Drone Loop
+  # creating an instance of the user
+  u = User.create(email: email, password: password, name: name, photo: photo, description: description)
+  # Drone Loop
   3.times do
     name = Faker::StarWars.droid
     description = Faker::VForVendetta.quote
     location = Faker::University.name
     photo = Faker::Pokemon.name
     price = rand(20..40)
-    d= Drone.new({name:name, description:description, location:location, photo:photo,price:price})
+    d = Drone.new(name: name, description: description, location: location, photo: photo, price: price)
     d.user = u
     d.save
     # Booking Loop
     2.times do
-    status = "pending"
-    start_date = Time.now
-    end_date = Time.parse ("Aug #{rand(1..31)} 2018")
-    b = Booking.create({user:u, drone:d, status:status, start_date:start_date, end_date:end_date})
+      status = "pending"
+      start_date = Time.now
+      end_date = Time.parse ("Aug #{rand(1..31)} 2018")
+      b = Booking.create(user: u, drone: d, status: status, start_date: start_date, end_date: end_date)
     end
   end
 end
