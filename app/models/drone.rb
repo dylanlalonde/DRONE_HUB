e0589beed0ece6e5b1f4aff7f6cbeff5b7a2224d
Dylan Lalonde <contact@dylanlalonde.com>
2018-06-14 11:39:21 +0200

    Search feature implemented

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -1,12 +1,21 @@
 class Drone < ApplicationRecord
+  include PgSearch
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+
+  # SEARCH FUNCTIONALITY:
+  pg_search_scope :search_by_name_and_description_and_category,
+    against: [ :name, :description, :category ],
+    using: {
+      tsearch: { prefix: true }
+    }
 
   # validates :name, presence: true
   # validates :description, presence: true
   # validates :location, presence: true
   # validates :photo, presence: true
   # validates :price, presence: true
+  # validates :category, presence: true  <==== NEW
 
 end
